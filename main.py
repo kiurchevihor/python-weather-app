8cab83f0afb2356ba62adf798c75cf454d0e5361
Ihor Kiurchev <mr.kyurchev007@gmai.com>
2025-02-04 18:04:28 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# from random import randint
+# choice = randint(1, 10)
+
+
+# if choice == 7:
+#     print("You're lucky =)")
+# else:
+#     print("Unlucky =(")
+
+
+# LOGICAL OPERATORS
+
+age = 7
+if age > 6 and age < 8:
+    print("You have to pay child price")
---
+++ mileage_converter.py
@@ -0,0 +1,4 @@
+your_km = int(input("How many kilometers did you run today, Sir?:"))
+MILE = 0.621371
+mile_convert = your_km * MILE
+print(f"Your {your_km} km run is {round(mile_convert, 2)} mi, Sir.")
